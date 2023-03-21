b8b120ee47302d6bc46528d08ff822a20f741be1
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:20:02 +0100

    feat: add plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/amqp/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using a RabbitMQ broker.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.amqp;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
