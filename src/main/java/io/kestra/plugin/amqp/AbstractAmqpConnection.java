f1055957582abda59bdd104d00d3d32796b81c41
Loïc Mathieu <loikeseke@gmail.com>
2023-06-01 11:44:30 +0200

    fix: configure AMQP connection with individual properties

--- src/main/java/io/kestra/plugin/amqp/AbstractAmqpConnection.java
+++ src/main/java/io/kestra/plugin/amqp/AbstractAmqpConnection.java
@@ -1,6 +1,7 @@
 package io.kestra.plugin.amqp;
 
 import com.rabbitmq.client.ConnectionFactory;
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.runners.RunContext;
 import lombok.EqualsAndHashCode;
@@ -10,6 +11,7 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 @SuperBuilder
 @ToString
@@ -18,31 +20,42 @@ import java.net.URI;
 @NoArgsConstructor
 public abstract class AbstractAmqpConnection extends Task implements AmqpConnectionInterface {
     private String url;
+    private String host;
+    private String port;
+    private String username;
+    private String password;
+    private String virtualHost;
 
     public ConnectionFactory connectionFactory(RunContext runContext) throws Exception {
-        URI amqpUri = new URI(runContext.render(url));
-
-        String auth = amqpUri.getUserInfo();
-        int pos = auth.indexOf(':');
-
+        if (url != null) {
+            parseFromUrl(runContext, url);
+        }
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(amqpUri.getHost());
-        factory.setPort(amqpUri.getPort());
-
-        String user = pos > 0 ? auth.substring(0, pos) : auth;
-        String pass = pos > 0 ? auth.substring(pos + 1) : "";
-
-        factory.setUsername(user);
-        factory.setPassword(pass);
-        
-        String virtualHost = amqpUri.getPath();
-        if (virtualHost.length() > 1) {
-            factory.setVirtualHost(virtualHost.substring(1));
-        }
+        factory.setHost(runContext.render(host));
+        factory.setPort(Integer.parseInt(runContext.render(port)));
+        factory.setUsername(runContext.render(username));
+        factory.setPassword(runContext.render(password));
+        factory.setVirtualHost(runContext.render(virtualHost));
 
         factory.setExceptionHandler(new AmqpExceptionHandler(runContext.logger()));
 
         return factory;
+    }
+
+    void parseFromUrl(RunContext runContext, String url) throws IllegalVariableEvaluationException, URISyntaxException {
+        URI amqpUri = new URI(runContext.render(url));
+
+        host = amqpUri.getHost();
+        port = String.valueOf(amqpUri.getPort());
+
+        String auth = amqpUri.getUserInfo();
+        int pos = auth.indexOf(':');
+        username = pos > 0 ? auth.substring(0, pos) : auth;
+        password = pos > 0 ? auth.substring(pos + 1) : "";
+
+        if (!amqpUri.getPath().equals("")) {
+            virtualHost = amqpUri.getPath();
+        }
     }
 }
--- src/main/java/io/kestra/plugin/amqp/AmqpConnectionInterface.java
+++ src/main/java/io/kestra/plugin/amqp/AmqpConnectionInterface.java
@@ -6,10 +6,38 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotNull;
 
 public interface AmqpConnectionInterface {
-    @NotNull
+
+    @Deprecated
+    @Schema(hidden = true)
+    String getUrl();
+
     @PluginProperty(dynamic = true)
     @Schema(
-        title = "The connection string"
+        title = "The broker host"
     )
-    String getUrl();
+    String getHost();
+
+    @PluginProperty(dynamic = true)
+    @Schema(
+        title = "The broker port"
+    )
+    String getPort();
+
+    @PluginProperty(dynamic = true)
+    @Schema(
+        title = "The broker virtual host"
+    )
+    String getVirtualHost();
+
+    @PluginProperty(dynamic = true)
+    @Schema(
+        title = "The broker username"
+    )
+    String getUsername();
+
+    @PluginProperty(dynamic = true)
+    @Schema(
+        title = "The broker password"
+    )
+    String getPassword();
 }
