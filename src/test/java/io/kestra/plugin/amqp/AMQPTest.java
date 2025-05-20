5bf2e7a6344cceed64d041737bc6e033dd93fa9c
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-05-20 09:58:59 +0200

    chore(test): inject main tenants on storage and runOne

--- src/test/java/io/kestra/plugin/amqp/AMQPTest.java
+++ src/test/java/io/kestra/plugin/amqp/AMQPTest.java
@@ -8,6 +8,7 @@ import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.serializers.JacksonMapper;
+import io.kestra.core.tenant.TenantService;
 import io.kestra.core.utils.IdUtils;
 import io.kestra.core.storages.StorageInterface;
 import io.kestra.plugin.amqp.models.Message;
@@ -287,6 +288,6 @@ class AMQPTest {
                     .data("value-" + i)
                     .build()));
         }
-        return storageInterface.put(null, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+        return storageInterface.put(TenantService.MAIN_TENANT, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
     }
 }
