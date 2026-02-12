cebba257d4f08355f66f190e12659349c0eeb0b3
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-12 17:08:03 +0100

    chore: update plugin categories (#114)
    * chore: update plugin categories
    
    * chore: fix plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

--- src/main/java/io/kestra/plugin/amqp/package-info.java
+++ src/main/java/io/kestra/plugin/amqp/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "Tasks that connect to RabbitMQ brokers to declare exchanges and queues, bind routing keys, publish messages, and consume them via tasks or triggers (batch or real time). Provide host/port/credentials/virtual host values for each connection, and set stop conditions such as maxDuration or maxRecords when consuming.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.amqp;
 
