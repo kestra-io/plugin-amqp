d16ae4d811d399ef8635e74d1e6a32909d9f6c3a
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-10 16:01:43 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/amqp/TriggerTest.java
+++ src/test/java/io/kestra/plugin/amqp/TriggerTest.java
@@ -2,13 +2,16 @@ package io.kestra.plugin.amqp;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableMap;
 
+import io.kestra.core.junit.annotations.EvaluateTrigger;
+import io.kestra.core.models.conditions.ConditionContext;
 import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.serializers.JacksonMapper;
@@ -17,38 +20,23 @@ import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.amqp.models.Message;
 import io.kestra.plugin.amqp.models.SerdeType;
 
-import reactor.core.publisher.Flux;
-
-import static io.kestra.core.utils.Rethrow.throwRunnable;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
-import io.kestra.core.models.conditions.ConditionContext;
-import java.util.Map;
 
 class TriggerTest extends AbstractTriggerTest {
+    @BeforeEach
+    void setUp() throws Exception {
+        publish();
+    }
+
     @Test
-    void flow() throws Exception {
-        CountDownLatch queueCount = new CountDownLatch(1);
-
-        Flux<Execution> receive = TestsUtils.receive(executionQueue, execution ->
-        {
-            queueCount.countDown();
-            assertThat(execution.getLeft().getFlowId(), is("trigger"));
-        });
-
-        this.run("trigger.yaml", throwRunnable(() ->
-        {
-            publish();
-
-            boolean await = queueCount.await(1, TimeUnit.MINUTES);
-            assertThat(await, is(true));
-
-            Integer trigger = (Integer) receive.blockLast().getTrigger().getVariables().get("count");
-
-            assertThat(trigger, greaterThanOrEqualTo(2));
-        }));
+    @EvaluateTrigger(flow = "flows/trigger.yaml", triggerId = "watch")
+    void run(Optional<Execution> optionalExecution) {
+        assertThat(optionalExecution.isPresent(), is(true));
+        Execution execution = optionalExecution.get();
+        assertThat((Integer) execution.getTrigger().getVariables().get("count"), greaterThanOrEqualTo(2));
     }
 
     @Test
