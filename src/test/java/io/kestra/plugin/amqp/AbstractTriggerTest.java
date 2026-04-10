d7c2c154e27cba897de7471b1726c10285957d74
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-10 16:01:43 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/amqp/AbstractTriggerTest.java
+++ src/test/java/io/kestra/plugin/amqp/AbstractTriggerTest.java
@@ -1,10 +1,7 @@
 package io.kestra.plugin.amqp;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.time.Instant;
 import java.util.Arrays;
-import java.util.Objects;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
@@ -12,43 +9,19 @@ import org.junit.jupiter.api.TestInstance;
 import com.google.common.collect.ImmutableMap;
 
 import io.kestra.core.junit.annotations.KestraTest;
-import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.property.Property;
-import io.kestra.core.queues.QueueFactoryInterface;
-import io.kestra.core.queues.QueueInterface;
-import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.FlowListeners;
 import io.kestra.core.runners.RunContextFactory;
-import io.kestra.core.runners.Worker;
 import io.kestra.core.serializers.JacksonMapper;
-import io.kestra.core.utils.IdUtils;
 import io.kestra.core.utils.TestsUtils;
-import io.kestra.jdbc.runner.JdbcScheduler;
 import io.kestra.plugin.amqp.models.Message;
-import io.kestra.scheduler.AbstractScheduler;
 
-import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
 
 @KestraTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class AbstractTriggerTest {
     @Inject
-    private ApplicationContext applicationContext;
-
-    @Inject
-    private FlowListeners flowListenersService;
-
-    @Inject
-    protected LocalFlowRepositoryLoader repositoryLoader;
-
-    @Inject
     protected RunContextFactory runContextFactory;
-
-    @Inject
-    @Named(QueueFactoryInterface.EXECUTION_NAMED)
-    protected QueueInterface<Execution> executionQueue;
 
     @BeforeAll
     void setUp() throws Exception {
@@ -81,20 +54,6 @@ abstract class AbstractTriggerTest {
         declareExchange.run(runContextFactory.of());
         createQueue.run(runContextFactory.of());
         queueBind.run(runContextFactory.of());
-    }
-
-    protected void run(String filename, Runnable runnable) throws IOException, URISyntaxException {
-        try (
-            AbstractScheduler scheduler = new JdbcScheduler(this.applicationContext, this.flowListenersService);
-            Worker worker = applicationContext.createBean(Worker.class, IdUtils.create(), 8, null);
-        ) {
-            worker.run();
-            scheduler.run();
-
-            repositoryLoader.load("null", Objects.requireNonNull(TriggerTest.class.getClassLoader().getResource("flows/" + filename)));
-
-            runnable.run();
-        }
     }
 
     protected Publish.Output publish() throws Exception {
